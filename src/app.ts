1d871b17b64ffe3d802881075e4cfdd2cbbe67ce
Shakti242 <shaktipatel2421@gmail.com>
2024-09-17 22:59:20 -0400

    errors setup

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
-import express from 'express'
+import express, { NextFunction, Response } from 'express'
+import { Request } from 'express';
+import { HttpError } from 'http-errors';
 
 const app = express();
 //Routes 
 app.get('/', (req, res) => {
     res.json({ message: "Welcome to elibs apis" });
 })
-
+app.use((err: HttpError, req:Request , res: Response, next: NextFunction) => {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const statusCode = err.statusCode || 500;
+});
 export default app;
