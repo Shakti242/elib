41e4be9871a3bd07acbc1abfd386cda21eccb4ef
Shakti242 <shaktipatel2421@gmail.com>
2024-09-19 20:14:55 -0400

    create user Router

--- src/user/userRouter.ts
+++ src/user/userRouter.ts
@@ -1,10 +1,7 @@
 import express from 'express';
-import { Request, Response } from 'express';
+import { createUser } from "./userController";
 
 const userRouter = express.Router();
 
-userRouter.post('/register', (req: Request, res: Response) => {
-    res.json({ message: 'User registered' });
-});
-
+userRouter.post('/register', createUser);
 export default userRouter;
